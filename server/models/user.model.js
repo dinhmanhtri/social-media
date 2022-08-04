2be5ff1ca8a05ee780c4834a398b8984d19ea01d
Manh Tri <mtri02.it@gmail.com>
2022-08-04 15:13:01 +0700

    #35 - feat: Implemented user model

---
+++ server/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profilePicture: String,
+    coverPicture: String,
+    about: String,
+    livesIn: String,
+    worksAt: String,
+    relationship: String,
+    followers: [],
+    following: [],
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default User;
