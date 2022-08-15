4a81a98734c8d4a0cc89bf4c505908502def06db
Manh Tri <mtri02.it@gmail.com>
2022-08-15 21:15:18 +0700

    #50-set-up-redux

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
+import { useDispatch } from "react-redux";
+import { logIn, signUp } from "../../actions/AuthAction.js";
 
 const Auth = () => {
-  let [isSignUp, setIsSignUp] = useState(false);
+  let [isSignUp, setIsSignUp] = useState(true);
+  const dispatch = useDispatch();
+
   const [data, setData] = useState({
     firstname: "",
     lastname: "",
@@ -22,9 +26,11 @@ const Auth = () => {
     e.preventDefault();
 
     if (isSignUp) {
-      if (data.password !== data.confirmpass) {
-        setConfirmPass(false);
-      }
+      data.password === data.confirmpass
+        ? dispatch(signUp(data))
+        : setConfirmPass(false);
+    } else {
+      dispatch(logIn(data));
     }
   };
 
