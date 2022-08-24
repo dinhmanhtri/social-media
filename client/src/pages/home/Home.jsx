0fb72e14cda4da70600b8efc8ab66be2d0fae0ab
Manh Tri <mtri02.it@gmail.com>
2022-08-24 22:52:00 +0700

    #59 - feat: Implement setup share post

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import PostSide from "../../components/PostSide/PostSide";
-import ProfileSide from "../../components/ProfileSide/ProfileSide";
+import PostSide from "../../components/PostSide/PostSide.jsx";
+import ProfileSide from "../../components/ProfileSide/ProfileSide.jsx";
 import RightSide from "../../components/RightSide/RightSide";
 import "./Home.css";
 
